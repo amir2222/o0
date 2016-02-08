2e60c4a4b0ca238fefd502fbb9e33606041ab4a6
Diss <Disslove77777@gmail.com>
2016-02-08 05:32:46 -0800

    Create Antilink.lua

---
+++ plugins/Antilink.lua
@@ -0,0 +1,14 @@
+local function run(msg)
+if not is_momod(msg) and msg.type = 'chat' then
+--chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+return 'هرگونه تبلیغ باعث حذف شما میشود.'
+     end 
+end 
+return {patterns = {
+".com",
+"".me,
+"http://",
+"https://",
+"adf.ly",
+"telgram.me",
+}, run = run}
